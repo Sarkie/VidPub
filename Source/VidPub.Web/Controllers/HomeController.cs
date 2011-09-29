216e2faa17ac854b015be3b07a6990da14a8ac8e
Sarkie <sarkie@gmail.com>
2011-09-29 12:44:34 +0100

    Added Logging, IoC

--- Source/VidPub.Web/Controllers/HomeController.cs
+++ Source/VidPub.Web/Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VidPub.Web.Infrastructure.Logging;
 
 namespace VidPub.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger logger;
+
+        public HomeController(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+
         public ActionResult Index()
         {
+            logger.LogInfo("Home Page Called");
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
